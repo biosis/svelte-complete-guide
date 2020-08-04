994b5b6576e5c2a5d2a15faf3c0a7b88f66eacc1
Biosis Lanjanirina <biosis.lanjanirina@gmail.com>
2020-08-04 09:16:06 +0300

    Svelte.js - The complete guide - Section O1

---
+++ section-01/src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  let name = "Bio";
+  let age = 41;
+
+  function incrementAge() {
+    age = age + 1;
+  }
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    /* text-transform: uppercase; */
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <h1>Hello {name}, your age is {age}</h1>
+  <button on:click={incrementAge}>Change age</button>
+</main>
