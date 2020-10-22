fb03b1f91f44a0e00b203ae130c1343c7541b715
Biosis Lanjanirina <biosis.lanjanirina@gmail.com>
2020-10-22 21:53:32 +0300

    Svelte.js - The coplete guide - Section 10 : Managing State & Data With Stores

---
+++ section-10/src/Cart/cart-store.js
@@ -0,0 +1,34 @@
+import {
+    writable
+} from 'svelte/store';
+
+const cart = writable([{
+        id: "p4",
+        title: "Test",
+        price: 9.99
+    },
+    {
+        id: "p5",
+        title: "Test",
+        price: 9.99
+    }
+]);
+
+const customCart = {
+    subscribe: cart.subscribe,
+    addItem: item => { 
+        cart.update(items => {
+            if (items.find(i => i.id === item.id)) {
+                return [...items];
+            }
+            return [...items, item];
+        })
+    },
+    removeItem: id => { 
+        cart.update((items) => {
+            return items.filter((i) => i.id !== id);
+        });
+    }
+}
+
+export default customCart;
