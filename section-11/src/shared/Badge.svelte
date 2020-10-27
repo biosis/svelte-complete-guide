9bd63799829f11e28d467a2ab07db963cb6db806
Biosis Lanjanirina <biosis.lanjanirina@gmail.com>
2020-10-27 13:40:19 +0300

    Svelte.js - The complete guide - Section 11 - Course Project - Stores

---
+++ section-11/src/shared/Badge.svelte
@@ -0,0 +1,17 @@
+<style>
+  span {
+    display: inline-block;
+    margin: 0 0.25rem;
+    padding: 0 0.5rem;
+    border-radius: 3px;
+    border: 1px solid #cf0056;
+    background: #cf0056;
+    color: white;
+    font-family: "Lato", sans-serif;
+    font-size: 0.8rem;
+  }
+</style>
+
+<span>
+  <slot />
+</span>
